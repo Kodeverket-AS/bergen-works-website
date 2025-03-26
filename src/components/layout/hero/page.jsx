1f002b6d178496379f7033d7b18d7c571c37810e
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-03-26 11:41:11 +0100

    hero 1.02

--- src/components/layout/hero/page.jsx
+++ src/components/layout/hero/page.jsx
@@ -2,21 +2,20 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col md:flex-row w-full h-auto'>
-        <div>
-            <img
-            src="/BW_Bygning.png"
-            alt="BergenWorks Lokaler"
-            className="rounded-3xl m-3 w-full h-auto md:pr-3 "/>
-        </div>
-        <div className='m-3 bg-gray-950 text-white rounded-3xl md:flex-row pr-3 w-1/2 max-h-96'>
-            <div className='flex flex-col'>
-                <div><h1 className='text-5xl p-5'>Bergen.Works</h1></div>
-                <div><p className='text-2xl pl-7'>Innovation in the heart of the city</p></div>
-                <div><button className='bg-amber-50 rounded-3xl border-2 text-black m-10 p-7'>Ta kontakt</button></div>
-            </div>
-            <div></div> 
-        </div>
+    <div className='p-3 flex items-center justify-center lg:h-1/3'>
+      <div className='flex flex-col w-full justify-center md:flex-row '>
+         <div>
+              <img className="rounded-3xl lg:w-auto" src="/BW_Bygning.png" alt="BergenWorks Lokaler" />
+          </div>
+
+          <div className='bg-gray-950 text-white rounded-3xl pt-3 md:pl-3 md:pt-0 '>
+             <div className='flex flex-col'>
+                  <div><h1 className='text-4xl p-5'>Bergen.Works</h1></div>
+                  <div><p className='text-1xl p-7'>Innovation in the heart of the city</p></div>
+                  <div><button className='bg-amber-50 rounded-3xl border-2 text-black m-10 p-5'>Ta kontakt</button></div>
+              </div>
+          </div>
+      </div>
     </div>
   )
 }
