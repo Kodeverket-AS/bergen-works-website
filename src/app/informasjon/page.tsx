016b9023374642715d6cc74d6826dd5f4e3955b2
Stein-B06 <Steinb06.kodehode@gmail.com>
2025-03-28 16:52:25 +0100

    second commit

--- src/app/informasjon/page.tsx
+++ src/app/informasjon/page.tsx
@@ -2,35 +2,67 @@ import Image from "next/image";
 
 export default function Information() {
   return (
-    <main className="m-[2em] flex flex-col items-center gap-4 ">
-      <div className=" bg-[#1D1D1D] rounded-2xl flex flex-col items-center h-[567px] w-[342px] gap-4  ">
-        <Image
-          src="/infoImage.png"
-          alt="Picture of logo and people in meetingroom"
-          width={294}
-          height={303}
-          className="mt-[1.5em]  rounded-2xl"
-          priority
-        />
-        <div className="text-[var(--text-light)] w-[294px] h-[117px] ">
-          <h1 className="font-bold text-[40px]">Co-work</h1>
-          <p className="text-base text-start ">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Perspiciatis ex fuga sint praesentium!
-          </p>
-          <button className="mt-[2em] w-[294px] h-[51px] rounded-sm py-4 px-8 bg-white text-[var(--text-dark)] md:hover:bg-accept-600 md:hover:text-[var(--text-light)] transition-colors duration-300 cursor-pointer">
-            Ta kontakt
-          </button>
+    <main className="flex flex-col items-center justify-center transition-all duration-700">
+      <div className=" bg-[#1D1D1D] lg:bg-transparent rounded-2xl flex flex-col items-center lg:flex-row justify-center h-[567px] w-[342px] md:h-[650px] md:w-[430px] lg:w-screen xl:w-[1300] lg:h-[411] gap-4 md:gap-6 lg:mb-[2em]">
+        <div className="md:bg-[#1D1D1D] lg:flex lg:items-center md:p-[1em] md:w-[400] rounded-2xl lg:w-[430] lg:h-[411]  ">
+          <Image
+            src="/infoImage.png"
+            alt="Picture of logo and people in meetingroom"
+            width={294}
+            height={303}
+            className="mt-[1.5em] md:mt-[1em] lg:mt-0 lg:p-[0.5em] object-cover rounded-2xl md:h-[383px] md:w-[400]"
+            priority
+          />
+        </div>
+        <div className="text-[var(--text-light)] w-[294px] h-full md:w-[400] rounded-2xl md:p-[1em] lg:w-[870] lg:h-[411] lg:bg-[#1D1D1D]">
+          <div className="xl:w-[753] xl:h-[45px] lg:ml-6">
+            <h1 className="font-bold text-[40px] md:text-[40px] md:-mt-10 lg:my-2 ">
+              Co-work
+            </h1>
+            <p className="text-base text-start ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+              fuga! Dolorem, veritatis praesentium?
+            </p>
+            <p className="hidden lg:block lg:w-[753]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              fuga quos culpa assumenda ab iste, atque facere aut fugit rerum
+              repudiandae eius praesentium alias ratione consequatur optio sit
+              placeat illum cum quo quidem corporis. Deserunt placeat vel
+              inventore laborum similique possimus repellat aperiam iste
+              molestias rem ipsam, voluptatibus nulla quasi animi illum
+              explicabo quae odio suscipit ipsa. Ipsam non tempore laudantium
+              veritatis deserunt est ex, eos architecto unde? Obcaecati iste
+              deserunt molestiae delectus ullam nemo.
+            </p>
+            <div className="w-full md:flex md:justify-center lg:justify-start md:mt-[1em] lg:mt-10">
+              <button className="text-base md:text-md mt-[2em] md:mt-[0.5em] w-[294px] h-[51px] lg:w-[148px] rounded-sm py-4 px-8 bg-white text-[var(--text-dark)] font-semibold  md:hover:bg-accept-600 md:hover:text-[var(--text-light)] transition-colors duration-300 cursor-pointer">
+                Ta kontakt
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="w-[342] h-[398] my-[1em]  flex flex-col justify-around ">
-        <div className=" flex justify-evenly items-end p-[2em] w-[342] h-[122] rounded-2xl bg-moss-200 text-[var(--text-dark)] ">
-          <Image src="/infoIcon1.png" alt="icon" width={58} height={58} />
-          <div className=" px-[1.5em] flex flex-col justify-end tracking-wide ">
-            <h1 className="text-2xl text-light font-bold leading-10">
+      <div className="w-[342] mt-[1em] h-[398] md:w-[430px] md:h-[430]  flex flex-col lg:flex-row lg:h-[194] lg:w-screen xl:w-[1300]  justify-around lg:justify-between    ">
+        <div className=" flex justify-evenly items-end lg:items-center p-[2em] w-[342] h-[122] lg:h-[194] lg:w-[400] md:w-[430px] rounded-2xl bg-moss-200 text-[var(--text-dark)] ">
+          <Image
+            className="w-[58px] h-[58px] md:w-[70px] md:h-[70px] lg:w-[100px] lg:h-[100px] xl:w-[130px] xl:h-[130px] object-cover"
+            src="/infoIcon1.png"
+            alt="icon"
+            width={200}
+            height={200}
+          />
+
+          <div className="overflow-hidden lg:mb-2 px-[1.5em] lg:px-[0.7em] flex flex-col justify-end lg:justify-center xl:justify-start h-full ">
+            <h1 className="text-2xl text-light font-bold leading-10 lg:leading-9 ">
               Fast plass
             </h1>
             <p className="">xxxx,- / mnd</p>
+            <p className="hidden xl:block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+              eius suscipit, placeat magnam fuga aspernatur, pariatur iste,
+              labore similique hic veritatis adipisci corrupti ducimus harum ea
+              facere veniam.
+            </p>
           </div>
           <div className="mb-[0.34em] h-full flex items-end">
             <svg
@@ -50,15 +82,28 @@ export default function Information() {
             </svg>
           </div>
         </div>
-        <div className=" flex justify-evenly items-end p-[2em] w-[342] h-[122] rounded-2xl bg-moss-600 text-[var(--text-light)] ">
-          <Image src="/infoIcon2.png" alt="icon" width={58} height={58} />
-          <div className=" px-[1.5em] flex flex-col justify-end tracking-wide ">
-            <h1 className="text-2xl text-light font-bold leading-10">
+        <div className="flex justify-evenly items-end lg:items-center p-[2em] lg:mx-[1.1em] w-[342] h-[122] lg:h-[194] lg:w-[400] md:w-[430px] rounded-2xl bg-moss-600 text-[var(--text-light)] ">
+          <Image
+            className="w-[58px] h-[58px] md:w-[70px] md:h-[70px] lg:w-[100px] lg:h-[100px] xl:w-[130px] xl:h-[130px] object-cover"
+            src="/infoIcon2.png"
+            alt="icon"
+            width={200}
+            height={200}
+          />
+
+          <div className="overflow-hidden lg:mb-2 px-[1.5em] lg:px-[0.7em] flex flex-col justify-end lg:justify-center xl:justify-start h-full">
+            <h1 className="text-2xl text-light font-bold leading-10 lg:leading-9">
               Åpen plass
             </h1>
             <p className="">xxxx,- / mnd</p>
+            <p className="hidden xl:block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+              eius suscipit, placeat magnam fuga aspernatur, pariatur iste,
+              labore similique hic veritatis adipisci corrupti ducimus harum ea
+              facere veniam.
+            </p>
           </div>
-          <div className="mb-[0.34em] h-full flex items-end">
+          <div className="mb-[0.34em] h-full flex justify-end items-end">
             <svg
               width="23"
               height="22"
@@ -68,7 +113,7 @@ export default function Information() {
             >
               <path
                 d="M2.28767 1.57211L21.1439 20.4283M21.1439 20.4283L21.1439 3.92913M21.1439 20.4283L4.6447 20.4283"
-                stroke="black"
+                stroke="white"
                 strokeWidth="3"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -76,15 +121,21 @@ export default function Information() {
             </svg>
           </div>
         </div>
-        <div className=" flex justify-evenly items-end p-[2em] w-[342] h-[122] rounded-2xl bg-moss-200 text-[var(--text-dark)] ">
-          <Image src="/infoIcon3.png" alt="icon" width={58} height={58} />
-          <div className=" px-[1.5em] flex flex-col justify-end tracking-wide ">
+        <div className=" flex justify-evenly items-end lg:items-start p-[2em] w-[342] h-[122] lg:h-[194] lg:w-[400] md:w-[430px] rounded-2xl bg-moss-200 text-[var(--text-dark)] ">
+          <Image
+            className="w-[58px] h-[58px] md:w-[70px] md:h-[70px] lg:w-[100px] lg:h-[100px] xl:w-[130px] xl:h-[130px] object-cover"
+            src="/infoIcon3.png"
+            alt="icon"
+            width={200}
+            height={200}
+          />
+          <div className="px-[1.5em] flex flex-col tracking-wide">
             <h1 className="text-2xl text-light font-bold leading-10">
               Inkubasjon
             </h1>
-            <p className="">Gratis for deltakere</p>
+            <p className="">Lorem, ipsum dolor.</p>
           </div>
-          <div className="mb-[0.34em] h-full flex items-end">
+          <div className="h-full flex items-end">
             <svg
               width="23"
               height="22"
