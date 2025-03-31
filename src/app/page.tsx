ed39b70aeb13e1bd5fdb397c47b6c5aad36654ff
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-03-31 11:29:06 +0200

    Partnere 1.0.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/layout/hero/page";
+import Partnere from "../components/layout/partnere/page";
 
 export default function Home() {
   return (
   <main>
     <Hero />
+    <Partnere />
   </main>
   );
 }
