bd25e9f8bf0d381b5da4945365cd19e84eea07c3
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-04-01 14:20:53 +0200

    Medlemskap 1.0.1

--- src/app/medlemskap/page.tsx
+++ src/app/medlemskap/page.tsx
@@ -1,3 +1,9 @@
+import Hero from '../../components/layout/medlemskap_hero/page';
+import Eget_Kontor from '../../components/layout/eget_komtor/page';
+
 export default function Incubator() {
-  return <main></main>;
+  return <main>
+    <Hero />
+    <Eget_Kontor />
+  </main>;
 }
