4811b89299797dea46ec7d00581dba438297fb25
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-04-01 14:58:11 +0200

    ikon fliser

--- src/app/medlemskap/page.tsx
+++ src/app/medlemskap/page.tsx
@@ -1,6 +1,6 @@
 import Hero from '../../components/layout/medlemskap_hero/page';
 import Eget_Kontor from '../../components/layout/eget_komtor/page';
-import Medlemskap_iconer from '../../components/layout/medlemskap_iconer';
+import Medlemskap_iconer from '../../components/layout/Medlemskap_iconer';
 
 export default function Incubator() {
   return <main>
--- src/components/layout/Medlemskap_iconer.jsx
+++ src/components/layout/Medlemskap_iconer.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Medlemskap_iconer = () => {
   return (
-    <div>
-      <div><img src="" alt="" /></div>
-      <div><img src="" alt="" /></div>
-      <div><img src="" alt="" /></div>
-      <div><img src="" alt="" /></div>
-      <div><img src="" alt="" /></div>
-      <div><img src="" alt="" /></div>
-      <div><img src="" alt="" /></div>
-      <div><img src="" alt="" /></div>
+    <div className='flex flex-row flex-wrap bg-white'>
+      <div ><img className='h-60' src="wi-fi_logo.png" alt="" /></div>
+      <div ><img className='h-60' src="printer_logo.png" alt="" /></div>
+      <div ><img className='h-60' src="telefon-rom_logo.png" alt="" /></div>
+      <div ><img className='h-60' src="telefon-rom_logo.png" alt="" /></div>
+      <div ><img className='h-60' src="moteror_logo.png" alt="" /></div>
+      <div ><img className='h-60' src="event_logo.png" alt="" /></div>
+      <div ><img className='h-60' src="kjokken_logo.png" alt="" /></div>
+      <div ><img className='h-60' src="nettvetk_logo.png" alt="" /></div>
     </div>
   )
 }
