bf279d6a6128839a8beadb0797abb3cf6865eb94
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-04-02 11:08:28 +0200

    fliser 1.0.8

--- src/components/layout/Medlemskap_iconer.jsx
+++ src/components/layout/Medlemskap_iconer.jsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 
 const Medlemskap_iconer = () => {
   return (
-<div className='flex flex-col items-center justify-center bg-gray-200 gap-3'>  
-  <div className='bg-white flex flex-row flex-wra gap-3'>
+<div className='flex flex-col items-center justify-center bg-gray-200 gap-3 mt-5 mb-5'>  
+  <div className='flex flex-row  gap-3'>
     <div className=''><img className='border-2 border-black h-60 w-auto' src="wi-fi_logo.png" alt="" /></div>
     <div className=''><img className='border-2 border-black h-60 w-auto' src="printer_logo.png" alt="" /></div>
     <div className=''><img className='border-2 border-black h-60 w-auto' src="telefon-rom_logo.png" alt="" /></div>
     <div className=''><img className='border-2 border-black h-60 w-auto' src="te_logo.png" alt="" /></div>
   </div>
-    <div className='bg-white flex flex-row flex-wra gap-3'>
+    <div className='flex flex-row gap-3'>
     <div className=''><img className='border-2 border-black h-60 w-auto' src="moterom_logo.png" alt="" /></div>
     <div className=''><img className='border-2 border-black h-60 w-auto' src="event_logo.png" alt="" /></div>
     <div className=''><img className='border-2 border-black h-60 w-auto' src="kjokken_logo.png" alt="" /></div>
