7600a298111237481d52f6b96c6ac8ef4b0f3459
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-04-02 13:24:32 +0200

    medlemskap 1.0.6

--- src/components/layout/kom_i_gang.jsx
+++ src/components/layout/kom_i_gang.jsx
@@ -4,12 +4,12 @@ import kontakt_form from './kontakt_form'
 const Kom_i_gang = () => {
   return (
     <div>
-        <div>
+        <div className='flex flex-col'>
             <div><h1>Kom i ang</h1></div>
             <div><p>Er du interessert i å bli en del av felleskapet eller har du noen spørsmål? Fyll ut kontakskjemaet og så kontakter vi deg fortløpende.</p></div>
             <div>
-                <div><Kontakt_form /></div>
-                <div><button>Ta kontakt</button></div>
+                <div></div>
+                <div></div>
             </div>
         </div>
         <div>
