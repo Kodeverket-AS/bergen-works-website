04d6a6f22a418a99daa6d15d52b8aaa74d2a78cc
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-04-04 11:44:07 +0200

    madlemskap 1.1.0 fliser

--- src/components/layout/kom_i_gang.jsx
+++ src/components/layout/kom_i_gang.jsx
@@ -5,11 +5,11 @@ const Kom_i_gang = () => {
     <div>
         <div className='flex flex-col mt-5 gap-5 md:flex-row'>
             <div className='flex flex-col gap-3'>
-                <div className='gap-3'>
+                <div className='gap-3 ml-5'>
                     <h1 className='text-4xl'>Kom i ang</h1>
                     <p className='text-2xl'>Er du interessert i å bli en del av felleskapet eller har du noen spørsmål? Fyll ut kontakskjemaet og så kontakter vi deg fortløpende.</p>
                 </div>
-                <div>
+                <div className='flex flex-col gap-3 ml-5'>
                     <h1>Kontakt form</h1>
                 </div>
                 
