5e0cfef067c34e940fe78db3aec22e34b20d8668
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-04-09 13:23:11 +0200

    Hero 2.0.1 text adjustment

--- src/components/layout/hero/page.jsx
+++ src/components/layout/hero/page.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
+import FasiliteterCard from "@/components/ui/FasiliteterCard"
 
 const Hero = () => {
   return (
-    <div className='p-3 flex items-center justify-center'>
-      <div className='flex flex-col w-full justify-center md:flex-row gap-3 md:gap-6 lg:gap-12'>
-
-         <div className='overflow-hidden rounded-3xl h-60 md:h-80 lg:h-96'>
-              <img className="w-full h-full object-cover object-center rounded-3xl lg:w-auto" src="/BW_Bygning.png" alt="BergenWorks Lokaler" />
-          </div>
-
-          <div className='bg-gray-950 text-white rounded-3xl pt-3 md:pl-3 md:pt-0 '>
-             <div className='flex flex-col'>
-                  <div><h1 className='text-4xl pt-4 pl-4'>Bergen.Works</h1></div>
-                  <div><p className='text-2xl p-7'>Innovation in the heart of the city</p></div>
-                  <div><button className='bg-amber-50 rounded-3xl border-2 text-black m-10 p-5'>Ta kontakt</button></div>
-              </div>
-          </div>
-      </div>
-    </div>
+    <div className="mb-10">
+            <FasiliteterCard
+              imageSrc="/BW_Bygning.png"
+              alt={"Image of building"}
+              headerText={"Bergen.Works"}
+              paragraphText={"Innovation in the heart of the city"}
+              buttonText={"Ta kontakt"}
+              buttonLink={"/"}
+              reverse={false}
+              cardStyle={""}
+              imageContainerStyle={"lg:w-3/5  lg:h-[450]"}
+              headerStyle={"text-4xl md:text-4xl xl:text-5xl  "}
+              textContainerStyle={"bg-black text-white lg:w-2/5 xl:h-[450]"}
+              paragraphStyle={"text-2xl lg:my-4"}
+              buttonStyle="bg-white text-black hover:bg-accept-600 hover:text-[var(--text-dark)] text-[var(--text-light)] "
+            />
+          </div>         
   )
 }
 
