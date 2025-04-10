2c6b0eb11648fcd70a8ac3bab578b58c323b1215
Monika <maam092024@gmail.com>
2025-04-10 15:36:22 +0200

    corrections

---
+++ src/components/Fasiliteter.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+
+const Fasiliteter = () => {
+  return (
+    <div className="w-full flex flex-col lg:flex-row  gap-4 my-8">
+      <div className="flex shadow-xl w-full lg:w-1/3 rounded-xl ">
+        <div className="flex lg:mt-auto  flex-col pl-4 pr-6">
+          <h1 className='text-xl font-semibold md:text-2xl lg:text-4xl p-4'>
+            Fasiliteter
+          </h1>
+          <p className="text-lg lg:text-3xl md:text-lg px-4 ">
+            Vi bryr oss om din bekvemmelighet og ditt humør
+          </p>
+          <p className="lg:text-2xl md:text-lg p-4">Det beste for kundene</p>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap gap-5 justify-around shadow-xl rounded-xl p-12 lg:w-2/3  ">
+        <div className="w-1/4 lg:w-1/5  flex flex-col justify-center  items-center  ">
+          <a href="">
+            <img src="wifi_icon.png" alt="" />
+          </a>
+          <p className="text-center text-xl lg:text-2xl">Wi-Fi</p>
+        </div>
+        <div className="w-1/4 lg:w-1/5  flex flex-col justify-center  items-center  ">
+          <a href="">
+            <img src="printer_icon.png" alt="" />
+          </a>
+          <p className="text-center text-xl lg:text-2xl">Printer</p>
+        </div>
+        <div className="w-1/4 lg:w-1/5  flex flex-col justify-center  items-center  ">
+          <a href="">
+            <img src="telephonRoon_icon.png" alt="" />
+          </a>
+          <p className="text-center text-xl lg:text-2xl">TelephonRoom</p>
+        </div>
+        <div className="w-1/4 lg:w-1/5  flex flex-col justify-center  items-center  ">
+          <a href="">
+            <img src="networking_icon.png" alt="" />
+          </a>
+          <p className="text-center text-xl lg:text-2xl">Networking</p>
+        </div>
+        <div className="w-1/4 lg:w-1/5  flex flex-col justify-center  items-center  ">
+          <a href="">
+            <img src="moterom_icon.png" alt="" />
+          </a>
+          <p className="text-center text-xl lg:text-2xl">Møterom</p>
+        </div>
+        <div className="w-1/4 lg:w-1/5  flex flex-col justify-center  items-center  ">
+          <a href="">
+            <img src="kjoken_icon.png" alt="" />
+          </a>
+          <p className="text-center text-xl lg:text-2xl">Kjøken</p>
+        </div>
+        <div className="w-1/4 lg:w-1/5  flex flex-col justify-center  items-center  ">
+          <a href="">
+            <img src="kaffe_tee_icon.png" alt="" />
+          </a>
+          <p className="text-center text-xl lg:text-2xl">Kaffe/Te</p>
+        </div>
+        <div className="w-1/4 lg:w-1/5  flex flex-col justify-center  items-center  ">
+          <a href="">
+            <img src="event_icon.png" alt="" />
+          </a>
+          <p className="text-center text-xl lg:text-2xl">Eventer/Kurs</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Fasiliteter
---
+++ src/components/Partnere.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Partnere = () => {
+  const partners = [
+    { logo: "DnB_Logo.png", url: "https://www.dnb.no" },
+    { logo: "simonsen_Logo.png", url: "https://svw.no/" },
+    { logo: "Escalon_Logo.png", url: "https://escalon.services/" },
+    { logo: "pallas_Logo.png", url: "https://pallas.no/" },
+    { logo: "Syde_Logo.png", url: "https://syde.no/" },
+    { logo: "Innovation_Logo.png", url: "" },
+    { logo: "Kodeverket_Logo.png", url: "https://www.kodeverketbergen.no/" },
+  ];
+
+  return (
+    <div className="w-full flex flex-col lg:flex-row  gap-4 my-8">
+      <div className="flex shadow-xl w-full lg:w-1/3 rounded-xl ">
+        <div className="flex lg:mt-auto mb-12 pr-4 flex-col ">
+          <h1 className="text-xl font-semibold md:text-2xl lg:text-4xl p-4">
+            Våre Partnere
+          </h1>
+          <p className="lg:text-2xl md:text-lg p-4">Vi bryr oss om partnerne våre</p>
+        </div>
+      </div>
+
+
+
+
+      <div className="flex flex-wrap justify-between shadow-xl rounded-xl p-4 lg:w-2/3">
+        {partners.map((partner, index) => (
+          <div
+            key={index}
+            className="w-1/3 lg:w-1/4 3xl:w-1/5 flex justify-center  items-center "
+          >
+            <a href={partner.url} target="_blank" rel="noopener noreferrer">
+              <img
+                src={partner.logo}
+                alt={partner.logo.split("_")[0]}
+                className="max-w-full h-auto"
+              />
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Partnere;
