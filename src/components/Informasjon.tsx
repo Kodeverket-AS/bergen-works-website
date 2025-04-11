83bd5ee3942ac73c9d06e02530a96593fec38e23
Stein-B06 <Steinb06.kodehode@gmail.com>
2025-04-11 09:20:38 +0200

    Added Tailwind and TS

--- src/components/Informasjon.tsx
+++ src/components/Informasjon.tsx
@@ -1,30 +1,31 @@
 import Image from "next/image";
-import InfoCard from "./ui/InfoCard";
+import { InfoCard } from "./ui/InfoCard";
 
 export default function InformationSection() {
   return (
-    <main className="my-10 flex flex-col items-center justify-center transition duration-500 ">
-      <div className="select-none  bg-[#1D1D1D] lg:bg-transparent rounded-2xl flex flex-col items-center lg:flex-row justify-center h-[567px] w-[342px] md:h-[650px] md:w-[430px] lg:w-full lg:h-[411] gap-4 md:gap-6 xl:gap-8 lg:mb-[2em]">
-        <div className="md:bg-[#1D1D1D] lg:flex lg:items-center md:p-[1em] md:w-[400] rounded-2xl lg:w-[400] lg:h-[411]  ">
+    <main className="my-10 flex flex-col items-center justify-center transition duration-500">
+      <div className="  bg-[#1D1D1D] lg:bg-transparent rounded-2xl flex flex-col items-center lg:flex-row justify-between h-[567px] w-[342px] md:h-[650px] md:w-[430px] lg:w-full lg:h-[411] xl:h-[450] gap-4 md:gap-5 xl:gap-9  lg:mb-[2em]">
+        <div className="select-none md:bg-[#1D1D1D] lg:flex lg:items-center lg:justify-center md:p-[1em] md:w-[400] rounded-2xl lg:w-[400] lg:h-full xl:w-[500] xl:h-[450]">
           <Image
             src="/infoImage.png"
             alt="Picture of logo and people in meetingroom"
-            width={294}
-            height={303}
-            className="mt-[1.5em] md:mt-[1em] lg:mt-0 lg:p-[0.5em] object-cover rounded-2xl md:h-[383px] md:w-[400]"
+            width={300}
+            height={300}
+            draggable={false}
+            className=" mt-[1.5em] md:mt-[1em] lg:mt-0 lg:p-[0.5em] object-cover rounded-2xl md:h-[360px] md:w-[450] xl:h-[400]"
             priority
           />
         </div>
-        <div className="text-[var(--text-light)] w-[294px] h-full md:w-[400] rounded-2xl md:p-[1em] lg:w-[850] lg:h-[411] lg:bg-[#1D1D1D]">
-          <div className="lg:w-[500]  xl:h-[45px] lg:px-10">
-            <h1 className="font-bold text-[40px] md:text-[40px] md:-mt-10 lg:my-2 ">
+        <div className="text-[var(--text-light)] w-[294px] h-full md:w-[400] rounded-2xl md:p-[1em] lg:w-10/12 xl:w-8/12 lg:h-[411] xl:h-[450] lg:bg-[#1D1D1D]">
+          <div className=" xl:h-[45px] lg:px-10">
+            <h1 className="font-bold text-[40px] xl:text-[50px] md:-mt-10 lg:my-2 ">
               Co-work
             </h1>
             <p className="text-sm md:text-base lg:hidden">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
               fuga! Dolorem, veritatis praesentium?
             </p>
-            <p className="hidden lg:block lg:w-[550] xl:w-[650] ">
+            <p className="hidden lg:block xl:text-lg p-2 ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
               fuga quos culpa assumenda ab iste, atque facere aut fugit rerum
               repudiandae eius praesentium alias ratione consequatur optio sit
@@ -35,7 +36,7 @@ export default function InformationSection() {
               veritatis deserunt est ex, eos architecto unde? Obcaecati iste
               deserunt molestiae delectus ullam nemo.
             </p>
-            <div className="w-full md:flex md:justify-center lg:justify-start mt-[1em] md:mt-[1.5em] lg:mt-[2em] xl:mt-[3em]">
+            <div className="w-full md:flex md:justify-center lg:justify-start mt-[1em] md:mt-[1.5em] lg:mt-[1em] xl:mt-[3em]">
               <button className="text-base md:text-md mt-[2em] md:mt-[0.5em]  w-[294px] h-[51px] lg:w-[148px] rounded-sm py-4 px-8 bg-white text-[var(--text-dark)] font-semibold  md:hover:bg-accept-600 md:hover:text-[var(--text-light)] transition-colors duration-300 cursor-pointer">
                 Ta kontakt
               </button>
@@ -44,7 +45,7 @@ export default function InformationSection() {
         </div>
       </div>
       <div className="w-[342] mt-[1em] h-[398] md:w-[430px] md:h-[430] flex flex-col lg:flex-row lg:h-[194] lg:w-full  justify-around xl:justify-center gap-5 xl:gap-10 ">
-        <div className="text-[var(--text-dark)]  ">
+        <div className="text-[var(--text-dark)] lg:w-1/3  ">
           <InfoCard
             bgColor="bg-moss-200"
             imageSrc="/infoIcon1.png"
@@ -54,7 +55,7 @@ export default function InformationSection() {
             link="/"
           />
         </div>
-        <div className="text-[var(--text-light)]">
+        <div className="text-[var(--text-light)] lg:w-1/3">
           <InfoCard
             bgColor="bg-moss-600"
             imageSrc="/infoIcon2.png"
@@ -64,7 +65,7 @@ export default function InformationSection() {
             link="/"
           />
         </div>
-        <div className="text-[var(--text-dark)]">
+        <div className="text-[var(--text-dark)] lg:w-1/3">
           <InfoCard
             bgColor="bg-moss-200"
             imageSrc="/infoIcon3.png"
--- src/components/ui/InfoCard.tsx
+++ src/components/ui/InfoCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import React from "react";
 import Link from "next/link";
 
-interface CardProps {
+interface InfoCardProps {
   imageSrc: string;
   title: string;
   description: string;
@@ -11,36 +11,37 @@ interface CardProps {
   link?: string;
 }
 
-const InfoCard: React.FC<CardProps> = ({
+export function InfoCard({
   imageSrc,
   title,
   description,
   bgColor,
   link = "/",
-}) => {
+}: InfoCardProps) {
   return (
     <div
-      className={` ${bgColor} flex justify-center items-center w-[342] h-[122] md:w-[430px] lg:h-[170] lg:w-[325] xl:w-[400px] rounded-2xl  transition-all duration-300 md:hover:scale-105`}
+      className={` ${bgColor} flex justify-center items-center w-full h-[140]  lg:h-[200]  rounded-2xl  transition-all duration-300 md:hover:scale-105`}
       style={{
         willChange: "transform",
       }}
     >
-      <div className="w-full flex justify-center items-center gap-x-10">
-        <div>
+      <div className="w-full flex justify-center items-center select-none ">
+        <div className="p-2">
           <Image
             src={imageSrc}
             alt={title}
             width={100}
             height={100}
-            className="mx-2 w-[70px] h-[70px] md:w-[80px] md:h-[80px] lg:w-[90px] lg:h-[90px] xl:w-[125px] xl:h-[125px]"
+            draggable={false}
+            className=" w-[80px] h-[80px] md:w-[90px] md:h-[90px] lg:w-[120px] lg:h-[120px] xl:w-[140px] xl:h-[140px]"
           />
         </div>
-        <div className=" flex flex-col justify-center w-1/2 ">
-          <h2 className="h-full text-2xl  font-bold ">{title}</h2>
+        <div className=" flex flex-col justify-center p-4 xl:px-8 ">
+          <h2 className="h-full text-2xl  font-bold  ">{title}</h2>
           <p className="mt-2 text-sm sm:text-base">{description}</p>
           <div className="mt-2 lg:mt-6">
             <Link href={link}>
-              <button className="text-sm rounded-sm py-1 px-4  bg-white text-[var(--text-dark)] font-semibold  md:hover:bg-accept-600 md:hover:text-[var(--text-light)] transition-colors duration-300 cursor-pointer">
+              <button className="text-sm lg:text-base rounded-lg py-1 px-4  bg-white text-[var(--text-dark)] font-semibold  md:hover:bg-accept-600 md:hover:text-[var(--text-light)] transition-colors duration-300 cursor-pointer">
                 Se mer
               </button>
             </Link>
@@ -49,6 +50,4 @@ const InfoCard: React.FC<CardProps> = ({
       </div>
     </div>
   );
-};
-
-export default InfoCard;
+}
