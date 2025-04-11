a4421aba4d5ac61335ed249745e2781fb9b12b59
Monika <maam092024@gmail.com>
2025-04-11 10:41:45 +0200

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "space-grotesk": ["Space Grotesk", "sans-serif"],
+      },
+      screens: {
+        "3xl": "1940px", 
+    },
+  },
+  plugins: [],
+}};
