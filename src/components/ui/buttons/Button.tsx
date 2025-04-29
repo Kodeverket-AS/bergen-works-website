b98ea16db16d74826b18c2b7da6e41502a5f5895
Stein-B06 <Steinb06.kodehode@gmail.com>
2025-04-29 09:28:51 +0200

    Added button component

---
+++ src/components/ui/buttons/Button.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Link from "next/link";
+
+type Variant = "primary" | "secondary";
+
+const variantStyles: Record<Variant, string> = {
+  primary:
+    "bg-moss-600 text-[var(--text-light)] hover:bg-[#1C2B14] cursor-pointer",
+  secondary:
+    "bg-white text-[var(--text-dark)] font-semibold hover:bg-gray-300 cursor-pointer",
+};
+
+type ButtonProps = {
+  children: React.ReactNode;
+  variant: Variant;
+  href?: string;
+  className: string;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+};
+
+export function Button({
+  children,
+  variant = "primary",
+  href,
+  className,
+  type = "button",
+  onClick,
+}: ButtonProps) {
+  const base = "inline-block px-4 py-2 rounded transition duration-300";
+  const styles = variantStyles[variant];
+  const combined = `${base} ${styles} ${className}`;
+
+  if (href) {
+    return (
+      <Link href={href} className={combined} role="button" tabIndex={0}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button type={type} onClick={onClick} className={combined}>
+      {children}
+    </button>
+  );
+}
