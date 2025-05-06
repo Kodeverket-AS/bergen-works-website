cafb9ba48352d50cda2b61ec074f1a7800185982
Kristoffer <kunkristoffer@gmail.com>
2025-05-06 17:56:12 +0200

    Created a new SSG/ISG version of articles

--- src/app/test/[slug]/page.tsx
+++ src/app/test/[slug]/page.tsx
@@ -1,10 +1,13 @@
-import { getArticle, getArticles } from "@/libs/sanity/fetch";
+import { PortableTextWrapper } from "@/components/layout/portableText/wrapper";
+import { ArticleImageCard } from "@/components/ui/cards/articleImage";
+import { getArticle, getArticles, urlFor } from "@/libs/sanity/fetch";
+import Image from "next/image";
 
 // Generate static pages for existing articles
 export async function generateStaticParams() {
   const articles = await getArticles();
   return articles.map((article) => ({
-    slug: article._id,
+    slug: article.slug?.current,
   }));
 }
 
@@ -15,6 +18,47 @@ export default async function page({
 }) {
   const slug = (await params).slug;
   const article = await getArticle(slug);
-  console.log(article);
-  return <div>article</div>;
+
+  if (!article) {
+    return <div>Not found :/</div>;
+  }
+
+  return (
+    <div className="container mx-auto text-moss-600 p-6 rounded-lg shadow-lg">
+      {article.background && (
+        <div className="relative mx-auto h-64 w-full bg-contain bg-no-repeat bg-center rounded-lg mb-6">
+          <Image
+            src={
+              article.background
+                ? (await urlFor(article.background)).url()
+                : "/infoIcon3.png"
+            }
+            alt="image"
+            fill
+            className="absolute object-contain"
+          />
+        </div>
+      )}
+      <h1 className="text-5xl font-semibold text-center text-moss-200 mb-6 leading-tight">
+        {article.title}
+      </h1>
+      <div className="flex justify-center items-center space-x-4 mb-6 text-sm text-moss-100">
+        {article?.releaseDate && (
+          <p>{new Date(article.releaseDate).toLocaleDateString()}</p>
+        )}
+        <span className="font-semibold">|</span>
+        <p>Autor: {article.author}</p>
+      </div>
+      <div className="mt-8 text-text-light text-lg leading-relaxed space-y-6">
+        <PortableTextWrapper value={article.articleBody} />
+      </div>
+      {article.images && (
+        <div className="mt-8 grid grid-cols-2 md:grid-cols-3 gap-6">
+          {article.images.map((image) => (
+            <ArticleImageCard key={image._key} image={image} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
