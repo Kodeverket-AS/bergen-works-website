e61fdd693911e1ec820a4896da09c952d91112e9
https://github.com/Enforsix <christian.gulkoder@gmail.com>
2025-05-07 11:53:21 +0200

    justert bilde

--- src/app/inkubator/page.tsx
+++ src/app/inkubator/page.tsx
@@ -6,39 +6,35 @@ import { Button } from "@/components/ui/buttons/Button";
 export default function Membership() {
     return (
         <main>
-            <section className=" flex flex-col lg:flex-row w-full">
-                <div className="lg:w-1/3 lg:pr-3">
+            <section className="flex flex-col w-full h-full items-stretch items-center md:flex-row gap-5">                
                 <Image
                     src="/Incubator.png"
                     alt="Incubator landing page img"
                     width={450}
                     height={450}
                     draggable={false}
-                    className="justify-center mb-10 lg:mb-0"                                                       
-                    />
-                </div>
-                <div className="bg-black text-white rounded-2xl lg:w-2/3 p-18">
-                    <h1 className="text-5xl font-bold lg:mt-15 mb-8">Inkubator</h1>
-                    <p className="mt-3 text-xl">Har du en Gründer i magen? Bergen.Works utvikler et nytt inkubasjonskonsept for de som ønsker å realisere dine drømmer og idéer! Ta turen innom oss for en uformell prat og en kopp kaffe! Bergen.Works er startet av gründere, for gründere; hos oss finner du kompetanse innen ethvert felt! Kom for kontorplass, bli for miljøet.</p>                    
+                    className="h-full w-full md:w-1/2 lg:w-1/3 object-cover object-center"                                                       
+                    />                
+                <div className="bg-black text-white pt-10 lg:pt-20 px-15 rounded-2xl shadow-lg w-full h-auto">
+                    <h1 className="text-4xl mb-8 md:text-5xl">Inkubator</h1>
+                    <p className="lg:text-2xl">Har du en Gründer i magen? Bergen.Works utvikler et nytt inkubasjonskonsept for de som ønsker å realisere dine drømmer og idéer! Ta turen innom oss for en uformell prat og en kopp kaffe! Bergen.Works er startet av gründere, for gründere; hos oss finner du kompetanse innen ethvert felt! Kom for kontorplass, bli for miljøet.</p>                    
                     <Button 
-                    className="w-full md:w-[200px] md:h-[50] mt-15 rounded-md" 
+                    className="mt-12 mb-10 lg:mt-20 w-full md:w-1/2 lg:w-1/3 rounded-2xl text-lg" 
                     variant="secondary"
                     href="/#contact-form">Ta kontakt</Button>
                 </div>                
             </section>
            
-            <section className="flex flex-col w-full lg:flex-row mt-10">
-                <div className="flex-1">
+            <section className="w-full h-full flex flex-col md:flex-row mt-10 mb-10 ">                
                     <Image
-                    className="rounded-2xl h-full object-cover"
+                    className="w-full m-[12px] md:w-1/2 md:object-cover md:object-center rounded-2xl"
                     src="/FasImg2.png"
                     alt="Incubator landing page img"
                     width={1300}
                     height={450}
                     draggable={false}
-                    />
-                </div>
-                <div className="flex-1 flex">
+                    />                
+                <div>
                     <KontaktForm/>
                 </div>
                 </section>
