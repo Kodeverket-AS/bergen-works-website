ea3abb438831d64d4b0ee59becbdb5860508a1c6
Stein-B06 <Steinb06.kodehode@gmail.com>
2025-05-15 10:16:05 +0200

    added notfound, tw

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+export default function NotFound() {
+  return (
+    <div className="h-screen mt-4">
+      <div className="flex font-space-grotesk overflow-hidden">
+        <h1 className="text-[clamp(10rem,30vw,30rem)] mr-2 h-[clamp(120px,22vw,350px)] flex items-center">
+          4
+        </h1>
+        <div className="mx-6 w-[clamp(120px,22vw,350px)] h-[clamp(120px,22vw,350px)] flex items-center justify-center bg-black p-2 lg:p-4 rounded-2xl ">
+          <div className="w-[90%] h-[90%] bg-white rounded "></div>
+        </div>
+        <h1 className="text-[clamp(10rem,30vw,30rem)] h-[clamp(120px,22vw,350px)] flex items-center  ml-2 ">
+          4
+        </h1>
+      </div>
+    </div>
+  );
+}
