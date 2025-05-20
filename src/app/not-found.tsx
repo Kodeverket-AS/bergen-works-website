c4510301eb0791c5c2e809fc806398fd61c2d2c5
Stein-B06 <Steinb06.kodehode@gmail.com>
2025-05-20 09:44:14 +0200

    added not-found, tw

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/buttons/Button";
+
 export default function NotFound() {
   return (
     <div className="h-screen mt-4">
@@ -11,6 +13,16 @@ export default function NotFound() {
         <h1 className="text-[clamp(10rem,30vw,30rem)] h-[clamp(120px,22vw,350px)] flex items-center  ml-2 ">
           4
         </h1>
+      </div>
+      <div className="flex flex-col items-center mt-10">
+        <h2 className="font-space-grotesk text-lg lg:text-2xl">
+          Beklager. Siden du ser etter eksisterer ikke.
+        </h2>
+        <div>
+          <Button className="rounded-lg mt-6 px-6" href="/" variant={"primary"}>
+            Hjem
+          </Button>
+        </div>
       </div>
     </div>
   );
