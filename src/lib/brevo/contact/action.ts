6101b61630d610f5f8609bd3e141a9131555a272
kristoffer <kunkristoffer@gmail.com>
2025-06-12 13:27:49 +0200

    Enabled validation in contact form server action

--- src/lib/brevo/contact/action.ts
+++ src/lib/brevo/contact/action.ts
@@ -1,14 +1,14 @@
 "use server";
 
 import { brevo } from "@/lib/brevo/app";
-import { ContactFormSchema } from "@/schema/brevo";
+import { contactFormSchema, ContactFormSchema } from "@/schema/brevo";
 
 export async function contactFormSignup(formData: ContactFormSchema) {
   // Validate form bindings and values
-  /* const validate = contactFormSchema.safeParse(content);
+  const validate = contactFormSchema.safeParse(formData);
   if (!validate.success) {
     return { code: 400, message: validate.error };
-  } */
+  }
 
   // Notify staff on new response trough e-mail
   const response = await brevo.sendEmail(formData, 2);
