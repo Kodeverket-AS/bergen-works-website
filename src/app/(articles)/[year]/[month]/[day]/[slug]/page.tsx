3b3ae6d1bb08d2e8b1304c93729436790834f802
kristoffer <kunkristoffer@gmail.com>
2025-07-01 19:26:00 +0200

    Updated placeholder with some example code for fetching wordpress data via apollo client

--- src/app/(articles)/[year]/[month]/[day]/[slug]/page.tsx
+++ src/app/(articles)/[year]/[month]/[day]/[slug]/page.tsx
@@ -1,3 +1,43 @@
-export default async function Page() {
-  return <div>test</div>;
+import { getWordpressArticle, getWordpressArticleSlugs } from "@/lib/apollo/fetch";
+import Link from "next/link";
+
+interface Slugs {
+  year: string;
+  month: string;
+  day: string;
+  slug: string;
+}
+
+export default async function Page({ params }: { params: Promise<Slugs> }) {
+  const { year, month, day, slug } = await params;
+  // Add zod validation of dates
+
+  // tmp fetch all available posts for generating links
+  const available = await getWordpressArticleSlugs();
+
+  // tmp null check on slug
+  if (!slug || slug.length === 0) {
+    return false;
+  }
+
+  // tmp get content of post for rendering
+  const content = await getWordpressArticle(slug);
+
+  // Create fallback if !content
+
+  return (
+    <main className="">
+      Path: {year}/{month}/{day}/{slug}
+      <hr />
+      <div className="flex flex-col">
+        {available?.map((item, index) => (
+          <Link key={index} href={`/1/1/1/${item.slug}`} className="hover:text-blue-500">
+            {item.title}
+          </Link>
+        ))}
+      </div>
+      <hr />
+      {content && <div dangerouslySetInnerHTML={{ __html: content }} />}
+    </main>
+  );
 }
