5e457e543598e101679ea534e3edd7e5930fb5ff
Monika <maam092024@gmail.com>
2025-07-02 13:45:24 +0200

    moss-200

--- src/app/medlemskap/MedlemskapServices.jsx
+++ src/app/medlemskap/MedlemskapServices.jsx
@@ -19,9 +19,9 @@ const MedlemskapServices = () => {
                 smallScreenReverse={false}
                 largeScreenReverse={false}
                 cardStyle="mb-5 gap-5  rounded-2xl"
-                imageContainerStyle="flex justify-center items-center md:w-2/5 h-60 md:h-auto"
+                imageContainerStyle="flex justify-center items-center md:w-2/5 h-60 md:h-auto border-2 border-moss-200 rounded-2xl"
                 imageStyle="w-60 md:w-70 h-60 md:h-70 xl:w-80 xl:h-80 m-auto"
-                textContainerStyle="bg-moss-100/80 text-black md:w-3/5"
+                textContainerStyle="bg-moss-200 text-black md:w-3/5"
                 headerStyle="text-3xl font-bold"
                 paragraphStyle=""
             />
@@ -39,9 +39,9 @@ const MedlemskapServices = () => {
                 smallScreenReverse={false}
                 largeScreenReverse={true}
                 cardStyle="mb-5 gap-5 "
-                imageContainerStyle="md:w-2/5 h-60 md:h-auto"
+                imageContainerStyle="md:w-2/5 h-60 md:h-auto border-2 border-moss-200 rounded-2xl"
                 imageStyle="w-60 md:w-70 h-60 md:h-70  xl:w-80 xl:h-80 m-auto"
-                textContainerStyle="bg-moss-200/80 text-black md:w-3/5"
+                textContainerStyle="bg-moss-200 text-black md:w-3/5"
                 headerStyle="text-3xl font-bold"
                 paragraphStyle=""
             />
@@ -59,9 +59,9 @@ const MedlemskapServices = () => {
                 smallScreenReverse={false}
                 largeScreenReverse={false}
                 cardStyle="mb-5 gap-5  rounded-2xl"
-                imageContainerStyle="md:w-2/5 h-60 md:h-auto"
+                imageContainerStyle="md:w-2/5 h-60 md:h-auto border-2 border-moss-200 rounded-2xl"
                 imageStyle="w-60 md:w-70 h-60 md:h-70 flex  xl:w-80 xl:h-80 m-auto"
-                textContainerStyle="bg-moss-400/30 text-black md:w-3/5"
+                textContainerStyle="bg-moss-200 text-black md:w-3/5"
                 headerStyle="text-3xl font-bold"
                 paragraphStyle=""
             />
