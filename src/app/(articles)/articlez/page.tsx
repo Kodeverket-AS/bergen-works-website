a2848c3b5ff8a445ebc73612efbf9072ed908766
Kristoffer <kunkristoffer@gmail.com>
2025-07-09 19:18:35 +0200

    wrapped items in grid with link tag

--- src/app/(articles)/articlez/page.tsx
+++ src/app/(articles)/articlez/page.tsx
@@ -1,4 +1,5 @@
 import { getAllWordpressPostDetails } from "@/lib/apollo/fetch";
+import Link from "next/link";
 
 export default async function Page() {
   const articles = await getAllWordpressPostDetails();
@@ -7,9 +8,11 @@ export default async function Page() {
       <h1>Artikler</h1>
       <div className="grid grid-cols-3 gap-2">
         {articles?.map((article) => (
-          <div key={article.slug} className="bg-accept-600 rounded-md">
-            <h3>{article.title}</h3>
-          </div>
+          <Link key={article.slug} href={article.uri}>
+            <div key={article.slug} className="bg-accept-600 rounded-md">
+              <h3>{article.title}</h3>
+            </div>
+          </Link>
         ))}
       </div>
     </main>
