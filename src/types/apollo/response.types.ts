e29bbbde5a80e777795cee0d60510c70188b61e2
kristoffer <kunkristoffer@gmail.com>
2025-07-14 22:18:51 +0200

    Improved apollo types with generics

--- src/types/apollo/response.types.ts
+++ src/types/apollo/response.types.ts
@@ -1,3 +1,53 @@
+interface PageInfo {
+  __typename: string;
+  endCursor: string;
+  hasNextPage: boolean;
+}
+
+interface Node<T> {
+  node: T | undefined;
+}
+interface Nodes<T> {
+  nodes: T[];
+}
+
+interface WordpressTags {
+  id: string;
+  slug: string;
+  name: string;
+}
+
+interface WordpressCategories {
+  id: string;
+  slug: string;
+  name: string;
+}
+
+interface WordpressImage {
+  sourceUrl: string;
+  altText: string;
+}
+
+export interface WordpressResponse<Key extends string, T> {
+  data: {
+    [K in Key]: T;
+  };
+}
+export interface WordpressPost {
+  __typename: "Post";
+  slug: string;
+  status: "published" | "private";
+  title: string;
+  uri: string;
+  date: Date;
+  modified: Date;
+  excerpt: string;
+  categories: Nodes<WordpressCategories>;
+  tags: Nodes<WordpressTags>;
+  isSticky: boolean;
+  featuredImage: Node<WordpressImage>;
+}
+
 export interface WordpressPostsResponse {
   data: {
     posts: {
@@ -28,11 +78,7 @@ export interface WordpressPostsResponse {
           };
         };
       }[];
-      pageInfo: {
-        __typename: string;
-        endCursor: string;
-        hasNextPage: boolean;
-      };
+      pageInfo: PageInfo;
     };
   };
 }
