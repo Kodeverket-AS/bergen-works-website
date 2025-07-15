e8b95927153d84ba32d3d07492a296e2f0652889
kristoffer <kunkristoffer@gmail.com>
2025-07-15 16:06:44 +0200

    Implemented new wordpress post fetch along with minor tweaks to code quality

--- src/app/(articles)/[year]/[month]/[day]/[slug]/page.tsx
+++ src/app/(articles)/[year]/[month]/[day]/[slug]/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import { notFound } from "next/navigation";
-import { getWordpressArticle, getWordpressPostsURIs } from "@/lib/apollo/fetch";
+import { getWordpressPostsURIs } from "@/lib/apollo/fetch";
+import { wpFetchPost } from "@/lib/apollo/fetch/post";
 import "@/assets/styles/frontend.min.css";
 
+// todo: replace fetch function
 export async function generateStaticParams() {
   const postsURIs = await getWordpressPostsURIs();
 
   // todo - check if dates are required constructors
   return postsURIs.map((uri) => ({
     slug: uri,
-  }))
+  }));
 }
 
 export default async function Page({
@@ -32,8 +34,13 @@ export default async function Page({
   if (isNaN(postDate.getTime())) return notFound();
 
   // Fetch content from wordpress site based on slug
-  const content = await getWordpressArticle(slug.trim());
-  if (!content) return notFound();
+  const result = await wpFetchPost(slug.trim());
+  if (result.error || !result.post) return notFound();
+
+  // Collect required data from result
+  const article = result.post;
+  const featuredImage = result.post.featuredImage.node || null;
+  // todo - replace placeholder image below
 
   // Format date for readability per client request
   const postDateFormatted = new Intl.DateTimeFormat("no-NO", {
@@ -44,28 +51,24 @@ export default async function Page({
 
   return (
     <main className="flex flex-col gap-4 pb-8 [&_h1]:text-[50px] [&_h2]:text-[25px] [&_h2]:font-light [&_p]:mb-7">
-      {content?.contentStyles && <style dangerouslySetInnerHTML={{ __html: content.contentStyles }} />}
-      <p className="capitalize">{postDateFormatted}</p>
-      {content?.featuredImage && (
-        <Image
-          src={content.featuredImage.node.sourceUrl}
-          alt={content.featuredImage.node.altText}
-          sizes="100vw"
-          style={{
-            width: "100%",
-            height: "auto",
-          }}
-          width={500}
-          height={300}
-        />
-      )}
-      <h1 className="self-center max-w-[768px]">{content?.title}</h1>
-      {content?.content && (
-        <div
-          dangerouslySetInnerHTML={{ __html: content.content }}
-          className="flex flex-col self-center gap-2 max-w-[768px]"
-        />
-      )}
+      <Image
+        src={featuredImage ? featuredImage.sourceUrl : "/KoV-ov.png"}
+        alt={featuredImage ? featuredImage.altText : "Alt text missing"}
+        sizes="100vw"
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+        width={500}
+        height={300}
+      />
+      <h1 className="self-center max-w-[768px]">{article.title}</h1>
+      <p className="self-center capitalize text-gray-500">{postDateFormatted}</p>
+      <div
+        dangerouslySetInnerHTML={{ __html: article.content }}
+        className="flex flex-col self-center gap-2 max-w-[768px]"
+      />
+      {article.contentStyles && <style dangerouslySetInnerHTML={{ __html: article.contentStyles }} />}
     </main>
   );
 }
