cff681e2d586e6db521b280e1c2d68164629eee4
kristoffer <kunkristoffer@gmail.com>
2025-07-15 17:22:43 +0200

    Fixed typos and removed unused wordpress types

--- src/types/apollo/response.types.ts
+++ src/types/apollo/response.types.ts
@@ -40,8 +40,8 @@ export interface WordpressPost {
   excerpt?: string;
   content?: string;
   contentStyles?: string;
-  categories: Nodes<WordpressCategories>;
-  tags: Nodes<WordpressTags>;
+  categories: Nodes<WordpressCategory>;
+  tags: Nodes<WordpressTag>;
   featuredImage: Node<WordpressImage>;
 }
 
@@ -59,6 +59,20 @@ export interface WordpressPostsResponse extends WordpressResponse {
 
 export interface WordpressPostsResult extends WordpressResponse {
   posts: WordpressPost[];
+}
+
+export interface WordpressPostsURIResponse {
+  posts: {
+    nodes: {
+      __typename: "Post";
+      uri: string;
+    }[];
+    pageInfo: {
+      __typename: string;
+      endCursor: string;
+      hasNextPage: boolean;
+    };
+  };
 }
 
 /**
@@ -90,88 +104,4 @@ export interface WordpressCategoriesResponse extends WordpressResponse {
 
 export interface WordpressCategoriesResult extends WordpressResponse {
   categories: WordpressCategory[];
-}
-
-export interface WordpressPostsResponse1 {
-  data: {
-    posts: {
-      nodes: {
-        __typename: "Post";
-        slug: string;
-        status: "published" | "private";
-        title: string;
-        uri: string;
-        date: Date;
-        modified: Date;
-        excerpt: string;
-        categories: {
-          nodes: {
-            name: string;
-          }[];
-        };
-        tags: {
-          nodes: {
-            name: string;
-          }[];
-        };
-        isSticky: boolean;
-        featuredImage: {
-          node: {
-            sourceUrl: string;
-            altText: string;
-          };
-        };
-      }[];
-      pageInfo: PageInfo;
-    };
-  };
-}
-
-export interface WordpressPostResponse1 {
-  data: {
-    post: {
-      title: string;
-      slug: string;
-      content: string;
-      contentStyles?: string;
-      featuredImage: {
-        node: {
-          sourceUrl: string;
-          altText: string;
-        };
-      };
-    };
-  };
-}
-
-export interface WordpressPostsURIResponse {
-  posts: {
-    nodes: {
-      __typename: "Post";
-      uri: string;
-    }[];
-    pageInfo: {
-      __typename: string;
-      endCursor: string;
-      hasNextPage: boolean;
-    };
-  };
-}
-
-export interface WordpressCategoriesResponse {
-  categories: {
-    nodes: {
-      id: string;
-      name: string;
-    }[];
-  };
-}
-
-export interface WordpressTagsResponse {
-  tags: {
-    nodes: {
-      id: string;
-      name: string;
-    }[];
-  };
 }
