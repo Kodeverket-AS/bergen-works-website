f511abc942277a1ceffed2f22f9610a74dbcca10
kristoffer <kunkristoffer@gmail.com>
2025-07-15 17:41:36 +0200

    Created list page for all available wordpress tags

--- src/app/(articles)/articles/tags/page.tsx
+++ src/app/(articles)/articles/tags/page.tsx
@@ -1,7 +1,24 @@
+import { wpFetchTags } from "@/lib/apollo/fetch/tags";
+import Link from "next/link";
+
 export default async function Page() {
+  const result = await wpFetchTags();
   return (
     <main>
-      <h1>List of all available tags</h1>
+      <h1 className="text-center text-2xl p-4">List of all available tags</h1>
+      <div className="flex justify-center">
+        <div className="w-full sm:w-1/2 flex flex-wrap justify-center gap-2">
+          {result.tags.map((tag) => (
+            <Link
+              key={tag.id}
+              href={"/articles/tags/" + tag.slug}
+              className="border rounded-md px-2 bg-moss-100 hover:bg-moss-200 duration-300"
+            >
+              {tag.name}
+            </Link>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
