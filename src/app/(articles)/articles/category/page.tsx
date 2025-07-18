a135140b59275bc37fa7ff3eb807e02f3554fb64
kristoffer <kunkristoffer@gmail.com>
2025-07-18 13:23:49 +0200

    Updated display language on article sub pages

--- src/app/(articles)/articles/category/page.tsx
+++ src/app/(articles)/articles/category/page.tsx
@@ -1,19 +1,19 @@
-import { wpFetchCategories } from "@/lib/apollo/fetch/categories";
-import Link from "next/link";
+import { wpFetchCategories } from '@/lib/apollo/fetch/categories';
+import Link from 'next/link';
 
 export default async function Page() {
   const result = await wpFetchCategories();
 
   return (
-    <main className="flex flex-col gap-4 pb-4">
-      <h1 className="text-center text-2xl">List of all available categories</h1>
-      <div className="flex justify-center">
-        <div className="w-full sm:w-1/2 flex flex-wrap justify-center gap-2">
+    <main className='flex flex-col gap-4 pb-4'>
+      <h1 className='text-center text-2xl'>Liste over alle tilgjengelige kategorier</h1>
+      <div className='flex justify-center'>
+        <div className='w-full sm:w-1/2 flex flex-wrap justify-center gap-2'>
           {result.categories.map((category) => (
             <Link
               key={category.id}
-              href={"/articles/category/" + category.slug}
-              className="border rounded-md px-2 bg-moss-100 hover:bg-moss-200 duration-300"
+              href={'/articles/category/' + category.slug}
+              className='border rounded-md px-2 bg-moss-100 hover:bg-moss-200 duration-300'
             >
               {category.name}
             </Link>
--- src/app/(articles)/articles/tags/page.tsx
+++ src/app/(articles)/articles/tags/page.tsx
@@ -1,18 +1,19 @@
-import { wpFetchTags } from "@/lib/apollo/fetch/tags";
-import Link from "next/link";
+import Link from 'next/link';
+import { wpFetchTags } from '@/lib/apollo/fetch/tags';
 
 export default async function Page() {
   const result = await wpFetchTags();
+
   return (
-    <main className="flex flex-col gap-4 pb-4">
-      <h1 className="text-center text-2xl">List of all available tags</h1>
-      <div className="flex justify-center">
-        <div className="w-full sm:w-1/2 flex flex-wrap justify-center gap-2">
+    <main className='flex flex-col gap-4 pb-4'>
+      <h1 className='text-center text-2xl'>Liste over alle tilgjengelige tags</h1>
+      <div className='flex justify-center'>
+        <div className='w-full sm:w-1/2 flex flex-wrap justify-center gap-2'>
           {result.tags.map((tag) => (
             <Link
               key={tag.id}
-              href={"/articles/tags/" + tag.slug}
-              className="border rounded-md px-2 bg-moss-100 hover:bg-moss-200 duration-300"
+              href={'/articles/tags/' + tag.slug}
+              className='border rounded-md px-2 bg-moss-100 hover:bg-moss-200 duration-300'
             >
               {tag.name}
             </Link>
