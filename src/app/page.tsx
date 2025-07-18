5380b13c42682ff6900b38d4c8f6ca6283784794
kristoffer <kunkristoffer@gmail.com>
2025-07-18 12:59:23 +0200

    Implemented an preview of articles on the front-page with a limited section of items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,29 @@
-"use client";
-import React from "react";
-import Hero from "../components/layout/navigation/hero/Hero";
-import Partnere from "../components/Partnere";
-import Fasiliteter from "../components/Fasiliteter";
-import InformationSection from "@/components/Informasjon";
-import ArticlesMain from "../components/ArticlesMain";
-/* import KontaktForm from "../components/KontaktForm"; */
-import Events from "../components/Events";
-import { ContactForm } from "@/components/forms/ContactForm";
-export default function Home() {
+import Link from 'next/link';
+import { wpFetchPosts } from '@/lib/apollo/fetch/posts';
+import Hero from '../components/layout/navigation/hero/Hero';
+import Partnere from '../components/Partnere';
+import Fasiliteter from '../components/Fasiliteter';
+import InformationSection from '@/components/Informasjon';
+import Events from '../components/Events';
+import { ContactForm } from '@/components/forms/ContactForm';
+import { ArticlesContainer } from '@/components/layout/articles/container';
+
+export default async function Home() {
+  const articles = await wpFetchPosts({ first: 3 });
+
   return (
-    <main className="">
+    <main className=''>
       <Hero />
       <InformationSection />
       <Fasiliteter />
       <Partnere />
-      <ArticlesMain />
+      <div className='flex flex-col items-center gap-4'>
+        <Link href='articles' className='text-3xl'>
+          Nyeste artikler
+        </Link>
+        <ArticlesContainer articles={articles.posts} />
+      </div>
       <Events />
-      {/* <KontaktForm /> */}
       <ContactForm />
     </main>
   );
