b6b0bf026f6f1ab85757faf693dfa514d648ee22
kristoffer <kunkristoffer@gmail.com>
2025-07-21 16:39:54 +0200

    removed redirect when visiting artikler page

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        pathname: "/images/**",
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/images/**',
       },
       {
-        protocol: "https",
-        hostname: "admin.bergen.works",
-        pathname: "/wp-content/uploads/**",
+        protocol: 'https',
+        hostname: 'admin.bergen.works',
+        pathname: '/wp-content/uploads/**',
       },
     ],
   },
@@ -19,18 +19,13 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: "/nyheter/:slug*",
-        destination: "https://admin.bergen.works/nyheter/:slug*",
+        source: '/nyheter/:slug*',
+        destination: 'https://admin.bergen.works/nyheter/:slug*',
         permanent: false,
       },
       {
-        source: "/artikler/:slug*",
-        destination: "https://admin.bergen.works/artikler/:slug*",
-        permanent: false,
-      },
-      {
-        source: "/blogg/:slug*",
-        destination: "https://admin.bergen.works/blogg/:slug*",
+        source: '/blogg/:slug*',
+        destination: 'https://admin.bergen.works/blogg/:slug*',
         permanent: false,
       },
     ];
