4c7c637ec8e8b3f006d9011af01d99451e4bf7d6
kristoffer <kunkristoffer@gmail.com>
2025-07-21 15:35:22 +0200

    Refactored article index page to use client and have filtering built in

--- src/app/(articles)/articles/page.tsx
+++ src/app/(articles)/articles/page.tsx
@@ -1,13 +1,87 @@
-import { wpFetchPosts } from '@/lib/apollo/fetch/posts';
-import { ArticlesContainer } from '@/components/layout/articles/container';
+'use client';
 
-export default async function Page() {
-  const articles = await wpFetchPosts({ first: 6 });
+import { type WordpressPost, type PageInfo, type WordpressPostsResult } from '@/types/apollo/response.types';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { ArticlesContainer } from '@/components/layout/articles/container';
+import RotateRightIcon from '@mui/icons-material/RotateRight';
+
+export default function Page() {
+  // Retrieve tags or categories from search parameters
+  const searchParams = useSearchParams();
+  const filterTag = searchParams.get('tag') || '';
+  const filterCategory = searchParams.get('category') || '';
+
+  // Store data
+  const [articles, setArticles] = useState<WordpressPost[]>([]);
+  const [pageInfo, setPageInfo] = useState<PageInfo>();
+  const [articlePerPage] = useState(9);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  async function loadMore() {
+    if (isLoading) return;
+    setIsLoading(true);
+
+    const response = await fetch(
+      `/api/wordpress?first=${articlePerPage}&after=${pageInfo?.endCursor}&tag=${filterTag}&category=${filterCategory}`
+    );
+    const data: WordpressPostsResult = await response.json();
+
+    if (data?.posts) setArticles((old) => [...old, ...data.posts]);
+    if (data?.pageInfo) setPageInfo(data.pageInfo);
+
+    setIsLoading(false);
+  }
+
+  // Handle data on page load
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setIsLoading(true);
+
+      const response = await fetch(
+        `/api/wordpress?first=${articlePerPage}&tag=${filterTag}&category=${filterCategory}`
+      );
+      const data: WordpressPostsResult = await response.json();
+
+      if (data?.posts) setArticles(data.posts);
+      if (data?.pageInfo) setPageInfo(data.pageInfo);
+
+      setIsLoading(false);
+    };
+    // fetchPosts();
+  }, [articlePerPage, filterTag, filterCategory]);
 
   return (
-    <main className='flex-1'>
-      <h1 className='text-center text-3xl pb-4'>Artikler</h1>
-      <ArticlesContainer articles={articles.posts} />
+    <main className='flex-1 flex flex-col gap-4 pb-4'>
+      <h1 className='text-center text-3xl'>Artikler fra Bergen Works</h1>
+      <span className='flex flex-col'>
+        {filterCategory && (
+          <p>
+            Vi fant <span className='font-bold'>{articles.length}</span> artikkler i kategorien{' '}
+            <span className='font-bold'>{filterCategory}</span>
+          </p>
+        )}
+        {filterTag && (
+          <p>
+            Vi fant <span className='font-bold'>{articles.length}</span> artikkler med tag{' '}
+            <span className='font-bold'>{filterTag}</span>
+          </p>
+        )}
+      </span>
+      <ArticlesContainer articles={articles} />
+      {isLoading && (
+        <p className='text-center p-4 animate-pulse'>
+          <RotateRightIcon className='animate-[spin_4s_ease-in-out_infinite] anim' /> Laster inn innhold...
+        </p>
+      )}
+      {pageInfo?.hasNextPage && (
+        <button disabled={!pageInfo?.hasNextPage} onClick={loadMore} className='p-2 bg-moss-200 hover:bg-moss-100'>
+          {isLoading ? 'Laster inn' : 'Last inn flere artikler'}
+        </button>
+      )}
+      {!pageInfo?.hasNextPage && articles.length > 6 && (
+        <p className='text-center'>Du har kommet til slutten av artikkel framvisning</p>
+      )}
     </main>
   );
 }
