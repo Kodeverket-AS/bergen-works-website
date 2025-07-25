e2d184c3d35adbbdd660b0d9cd5e0d88e18bd1b0
kristoffer <kunkristoffer@gmail.com>
2025-07-25 15:22:18 +0200

    Adding hover state to article tags component

--- src/components/ui/tags/articleTag.tsx
+++ src/components/ui/tags/articleTag.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export function ArticleTag(tag: WordpressTag) {
   return (
-    <Link href={'/artikler?tag=' + tag.slug} className='px-2 rounded-md bg-moss-100'>
+    <Link href={'/artikler?tag=' + tag.slug} className='px-2 rounded-md bg-moss-100 hover:bg-moss-200 duration-200'>
       {tag.name.startsWith('#') ? tag.name : '#' + tag.name}
     </Link>
   );
