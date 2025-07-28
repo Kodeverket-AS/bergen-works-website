44e918171ca435d90287da25898aee676b0046b3
kristoffer <kunkristoffer@gmail.com>
2025-07-28 16:25:00 +0200

    Created an event page for testing new wpfetchevents functions

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,6 +1,16 @@
-import { wpFetchEvents } from "@/lib/apollo/fetch/events/events";
+// import { wpFetchEvents } from '@/lib/apollo/fetch/events/events';
+import { wpFetchEventsByDateRange } from '@/lib/apollo/fetch/events/eventsByDate';
 
 export default async function EventsPage() {
-  const result = await wpFetchEvents()
+  const response = await wpFetchEventsByDateRange({ startDate: '2025-08-01 23:59:59', endDate: '2025-08-31 23:59:59' });
+
+  if (response.error) return <main>{response.error}</main>;
+
+  const events = response.events;
+
+  events.forEach((event) => {
+    console.log(event.title);
+  });
+
   return <main>test</main>;
 }
