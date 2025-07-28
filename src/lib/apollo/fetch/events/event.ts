9f980041bc479a48166d75c6250a0380d6301f1f
kristoffer <kunkristoffer@gmail.com>
2025-07-28 16:23:41 +0200

    Added function for fetching a single wp event based on slug

---
+++ src/lib/apollo/fetch/events/event.ts
@@ -0,0 +1,82 @@
+'use server';
+
+import { type WpEventResponse } from '@/types/apollo/events.types';
+import { ApolloError, gql } from '@apollo/client';
+import apolloClient from '@/lib/apollo/client';
+
+const QUERY = gql`
+  query event($id: ID = "") {
+    event(id: $id, idType: SLUG) {
+      title
+      slug
+      allDay
+      excerpt
+      date
+      endDate
+      eventsCategories {
+        nodes {
+          id
+          slug
+          name
+        }
+      }
+      tags {
+        nodes {
+          id
+          slug
+          name
+        }
+      }
+      featured
+      featuredImage {
+        node {
+          uri
+          altText
+          description
+          status
+          caption
+          mediaDetails {
+            height
+            width
+          }
+          sourceUrl
+        }
+      }
+      hideFromUpcoming
+      organizers {
+        nodes {
+          featuredImage {
+            node {
+              uri
+              altText
+            }
+          }
+          slug
+          link
+          title
+          website
+        }
+      }
+    }
+  }
+`;
+
+export async function wpFetchEvent(slug: string) {
+  try {
+    const response = await apolloClient.query<WpEventResponse>({
+      query: QUERY,
+      variables: { id: slug },
+    });
+
+    if (response.error) return { event: null, error: response.error.message };
+
+    return { event: response.data.event, error: null };
+  } catch (error) {
+    // Construct error message based on error type
+    const errorMessage =
+      error instanceof ApolloError
+        ? error.message
+        : 'Unknown error occoured while fetching wordpress event, contact site admin';
+    return { event: null, error: errorMessage };
+  }
+}
