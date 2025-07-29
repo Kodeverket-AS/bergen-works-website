44f36202e8d830202c81062862e218f0865efbb6
kristoffer <kunkristoffer@gmail.com>
2025-07-29 16:37:38 +0200

    Created a WpEvent preview card and a container for use on the landing page

---
+++ src/components/layout/events/upcomingEvents.tsx
@@ -0,0 +1,19 @@
+import { type WpEvent } from '@/types/apollo/events.types';
+import { EventCardPreview } from '@/components/ui/cards/eventPreview';
+import Link from 'next/link';
+
+export function UpcomingEvents({ events }: { events: WpEvent[] }) {
+  const slizedArray = events.slice(0, 3);
+  return (
+    <div className='flex flex-col items-center gap-4 my-8'>
+      <Link href='/events' className='text-3xl'>
+        Neste hendelser
+      </Link>
+      <div className='w-full grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+        {slizedArray.map((event) => (
+          <EventCardPreview key={event.slug} {...event} />
+        ))}
+      </div>
+    </div>
+  );
+}
