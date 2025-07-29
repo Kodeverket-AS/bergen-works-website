ecbddbe98638333f47977a876d20248304fc4f80
kristoffer <kunkristoffer@gmail.com>
2025-07-29 16:59:07 +0200

    Created temporary wp events index page for quick navigation during development

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,16 +1,23 @@
-// import { wpFetchEvents } from '@/lib/apollo/fetch/events/events';
-import { wpFetchEventsByDateRange } from '@/lib/apollo/fetch/events/eventsByDate';
+import { wpFetchEvents } from '@/lib/apollo/fetch/events/events';
+import Link from 'next/link';
 
 export default async function EventsPage() {
-  const response = await wpFetchEventsByDateRange({ startDate: '2025-08-01 23:59:59', endDate: '2025-08-31 23:59:59' });
+  // Fetch all events from wordpress
+  const response = await wpFetchEvents();
 
+  // Print error message if we encounter it
   if (response.error) return <main>{response.error}</main>;
 
-  const events = response.events;
-
-  events.forEach((event) => {
-    console.log(event.title);
-  });
-
-  return <main>test</main>;
+  return (
+    <main className='flex flex-col gap-4 mb-8'>
+      <h1 className='text-center'>Alle events</h1>
+      <div className='flex flex-col gap-2'>
+        {response.events.map((event) => (
+          <Link key={event.slug} href={'/event/' + event.slug}>
+            {event.title}
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
 }
