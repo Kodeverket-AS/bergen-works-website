a8fbd595b4e93a620c6146ce351037218f402560
Kristoffer <kunkristoffer@gmail.com>
2025-07-30 15:35:23 +0200

    Created a small component for combining icons and links

---
+++ src/components/ui/links/iconLink.tsx
@@ -0,0 +1,51 @@
+import { ComponentProps, ReactNode } from 'react';
+import Link from 'next/link';
+import LinkIcon from '@mui/icons-material/Link';
+
+interface IconLinkProps extends ComponentProps<'span'> {
+  /** The link you want to use */
+  link: string;
+  /** Optional label for the link */
+  label?: string;
+  /** The Icon you want to use as a React component */
+  icon?: ReactNode;
+  /** Optional field for styling default icon */
+  iconStyle?: ComponentProps<'a'>['className'];
+  /** Optional field for defining link text styles */
+  linkStyle?: ComponentProps<'a'>['className'];
+  /** Optional field for defining link type. Example:  */
+  linkType?: 'tel' | 'mailto';
+  /** Set this to true if the link is to an external adress */
+  isExternal?: boolean;
+}
+
+/**
+ * Small wrapper for creating links with icons
+ *
+ * Optional fields: label, icon, iconStyle, linkStyle & isExternal
+ */
+export function IconLink({
+  icon,
+  link,
+  label,
+  linkStyle,
+  linkType,
+  isExternal = false,
+  className,
+  ...rest
+}: IconLinkProps) {
+  return (
+    <span {...rest} className={`flex grow gap-2 bg-orange-200 ${className}`}>
+      {icon ? icon : <LinkIcon />}
+      {isExternal || linkType ? (
+        <a href={`${linkType ? linkType + ':' : ''}${link}`} className={`truncate ${linkStyle}`}>
+          {label ? label : link}
+        </a>
+      ) : (
+        <Link href={link} className={`truncate ${linkStyle}`}>
+          {label ? label : link}
+        </Link>
+      )}
+    </span>
+  );
+}
