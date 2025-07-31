3dc983ed44bb894f8c223cf7ae52ca40019a9b06
kristoffer <kunkristoffer@gmail.com>
2025-07-31 17:17:31 +0200

    Minor changes to event orginizer card style on mobile sizes

--- src/components/ui/cards/eventOrginizer.tsx
+++ src/components/ui/cards/eventOrginizer.tsx
@@ -7,19 +7,24 @@ import { IconLink } from '@/components/ui/links/iconLink';
 export function EventOrginizerCard(organizer: WpOrganizer) {
   return (
     <div className='flex gap-4 p-2 rounded-md bg-gray-50 hover:bg-gray-100 duration-200'>
-      <div className='relative h-full aspect-square shrink rounded-full bg-gray-200'>
+      <div className='hidden sm:block relative h-full aspect-square shrink rounded-full bg-gray-200'>
         <Image
           src={organizer.featuredImage?.node?.sourceUrl || '/KoV-ov.png'}
           alt={organizer.featuredImage?.node?.altText || 'Alt text missing'}
           fill
-          sizes='200vw'
+          sizes='10vw'
           className='object-cover rounded-full'
         />
       </div>
-      <span className='relative flex grow flex-col overflow-hidden'>
+      <span className='relative flex grow flex-col overflow-hidden text-sm'>
         <h3 className='text-xl font-bold'>{organizer.title}</h3>
         {organizer.excerpt && (
-          <div className='italic py-1' dangerouslySetInnerHTML={{ __html: organizer.excerpt }}></div>
+          <div
+            className='italic py-1 line-clamp-2'
+            dangerouslySetInnerHTML={{
+              __html: organizer.excerpt + organizer.excerpt + organizer.excerpt + organizer.excerpt,
+            }}
+          ></div>
         )}
         {organizer.phone && <IconLink icon={<CallIcon />} link={organizer.phone} linkType='tel' />}
         {organizer.email && <IconLink icon={<AlternateEmailIcon />} link={organizer.email} linkType='mailto' />}
