ae1d52f42854ec5a0c7c634635bc217302794543
Kristoffer <kunkristoffer@gmail.com>
2025-08-05 14:16:55 +0200

    Added page for event calendar variant 2

---
+++ src/app/events2/page.tsx
@@ -0,0 +1,16 @@
+import { wpFetchEvents } from '@/lib/apollo/fetch/events/events';
+import { EventCalendar } from '@/components/ui/calendar/eventCalendarV2';
+
+export default async function EventsPage() {
+  // Fetch all events from wordpress
+  const response = await wpFetchEvents();
+
+  // Print error message if we encounter it
+  if (response.error) return <main>{response.error}</main>;
+
+  return (
+    <main className='flex flex-col pb-8'>
+      <EventCalendar events={response.events} />
+    </main>
+  );
+}
