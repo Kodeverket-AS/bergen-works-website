6adb34e489c8441a793bcd72c88eaea73424c0e1
kristoffer <kunkristoffer@gmail.com>
2025-08-08 18:32:52 +0200

    disabled some colors in getpresetcolors fn

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -30,12 +30,12 @@ export function getPresetColorFromString(categoryName: string = '', alpha: numbe
   const approvedColors: Record<string, string> = {
     red: 'rgba(248, 113, 113, ALPHA)',
     yellow: 'rgba(251, 191, 36, ALPHA)',
-    green: 'rgba(52, 211, 153, ALPHA)',
+    /* green: 'rgba(52, 211, 153, ALPHA)', */
     blue: 'rgba(96, 165, 250, ALPHA)',
     purple: 'rgba(167, 139, 250, ALPHA)',
     pink: 'rgba(244, 114, 182, ALPHA)',
     orange: 'rgba(251, 146, 60, ALPHA)',
-    emerald: 'rgba(74, 222, 128, ALPHA)',
+    /* emerald: 'rgba(74, 222, 128, ALPHA)', */
     sky: 'rgba(56, 189, 248, ALPHA)',
     violet: 'rgba(192, 132, 252, ALPHA)',
   };
