842279ff2ebfca704dbb9de3a04d5630ebaa47a5
kristoffer <kunkristoffer@gmail.com>
2025-08-08 19:36:04 +0200

    Separated apollo functions into server side and client side for optimizing build

---
+++ src/lib/apollo/server/client.ts
@@ -0,0 +1,11 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+
+const apolloClientServer = new ApolloClient({
+  ssrMode: true,
+  link: createHttpLink({
+    uri: process.env.WORDPRESS_GRAPHQL_URL,
+  }),
+  cache: new InMemoryCache(),
+});
+
+export default apolloClientServer;
