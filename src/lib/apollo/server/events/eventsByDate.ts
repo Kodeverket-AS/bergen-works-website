e8672cc92b5346eb465aebacb24a356d32b544b8
kristoffer <kunkristoffer@gmail.com>
2025-08-08 19:50:51 +0200

    Added server side version of wp event fetch functions

---
+++ src/lib/apollo/server/events/eventsByDate.ts
@@ -0,0 +1,129 @@
+'use server';
+
+import { WpEvent, type WpEventsResponse } from '@/types/apollo/events.types';
+import { ApolloError, gql } from '@apollo/client';
+import { isValidDateString } from '@/utils/dates';
+import apolloClientServer from '@/lib/apollo/server/client';
+
+const QUERY = gql`
+  query events($first: Int = 100, $after: String = null, $startDate: String!, $endDate: String!) {
+    events(first: $first, after: $after, where: { runsBetween: { start: $startDate, end: $endDate } }) {
+      nodes {
+        id
+        slug
+        title
+        content
+        excerpt
+        url
+        cost
+        hideFromUpcoming
+        featured
+        featuredImage {
+          node {
+            sourceUrl
+            altText
+          }
+        }
+        duration
+        modified
+        showMap
+        date
+        startDate
+        endDate
+        duration
+        allDay
+        eventsCategories {
+          nodes {
+            id
+            slug
+            name
+          }
+        }
+        tags {
+          nodes {
+            id
+            slug
+            name
+          }
+        }
+        venue {
+          title
+          address
+          city
+          country
+          zip
+          content
+          excerpt
+          url
+          featuredImage {
+            node {
+              sourceUrl
+              altText
+            }
+          }
+        }
+        organizers {
+          nodes {
+            title
+            slug
+            content
+            excerpt
+            phone
+            email
+            website
+            featuredImage {
+              node {
+                sourceUrl
+                altText
+              }
+            }
+          }
+        }
+      }
+      pageInfo {
+        __typename
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+    }
+  }
+`;
+
+interface WpQueryOptions {
+  first?: number;
+  after?: string | null;
+  startDate: string;
+  endDate: string;
+}
+
+export async function wpFetchEventsByDateRangeServer(
+  queryOptions: WpQueryOptions
+): Promise<{ events: WpEvent[]; error: string | null }> {
+  try {
+    const { first = 100, after = null, endDate, startDate } = queryOptions;
+
+    if (!isValidDateString(startDate) || !isValidDateString(endDate))
+      return {
+        events: [],
+        error: 'Invalid date strings',
+      };
+
+    const response = await apolloClientServer.query<WpEventsResponse>({
+      query: QUERY,
+      variables: { first, after, startDate, endDate },
+    });
+
+    if (response.error) return { events: [], error: response.error.message };
+
+    return { events: response.data.events?.nodes || [], error: null };
+  } catch (error) {
+    // Construct error message based on error type
+    const errorMessage =
+      error instanceof ApolloError
+        ? error.message
+        : 'Unknown error occoured while fetching wordpress post, contact site admin';
+    return { events: [], error: errorMessage };
+  }
+}
