75472e43c159f95e3bb7938cf0c2b271e61c2c21
kristoffer <kunkristoffer@gmail.com>
2025-08-11 14:38:36 +0200

    Changed apollo client from singleton to avoid cache sharing for improving tag invalidation

--- src/lib/apollo/server/client.ts
+++ src/lib/apollo/server/client.ts
@@ -1,13 +1,23 @@
 'server only';
 
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, type NormalizedCacheObject } from '@apollo/client';
+import { relayStylePagination } from '@apollo/client/utilities';
 
-const apolloClientServer = new ApolloClient({
-  ssrMode: true,
-  link: createHttpLink({
-    uri: process.env.WORDPRESS_GRAPHQL_URL,
-  }),
-  cache: new InMemoryCache(),
-});
-
-export default apolloClientServer;
+export function getApolloClient(): ApolloClient<NormalizedCacheObject> {
+  return new ApolloClient({
+    ssrMode: true,
+    link: new HttpLink({
+      uri: process.env.WORDPRESS_GRAPHQL_URL!,
+    }),
+    cache: new InMemoryCache({
+      typePolicies: {
+        RootQuery: {
+          fields: {
+            events: relayStylePagination(['where']),
+            posts: relayStylePagination(['where']),
+          },
+        },
+      },
+    }),
+  });
+}
