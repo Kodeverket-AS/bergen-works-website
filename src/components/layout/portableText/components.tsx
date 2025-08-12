de847618cd41b9b1d0bb6760eb972c1ce1c029a3
Kristoffer <kunkristoffer@gmail.com>
2025-08-12 14:01:12 +0200

    Updating sanity urlFor import in sub components

--- src/components/layout/portableText/components.tsx
+++ src/components/layout/portableText/components.tsx
@@ -1,18 +1,18 @@
-import { type SanityImageAsset } from "@/types/sanity/sanity.types";
-import { type PortableTextReactComponents } from "next-sanity";
-import { urlFor } from "@/libs/sanity/fetch";
-import Image from "next/image";
-import Link from "next/link";
+import { urlFor } from '@/libs/sanity/client';
+import { type SanityImageAsset } from '@/types/sanity/sanity.types';
+import { type PortableTextReactComponents } from 'next-sanity';
+import Image from 'next/image';
+import Link from 'next/link';
 
 async function PortableTextImage(asset: SanityImageAsset) {
   return (
     <Image
-      alt={asset.altText || " "}
-      loading="lazy"
+      alt={asset.altText || ' '}
+      loading='lazy'
       src={(await urlFor(asset)).url()}
       width={500}
       height={200}
-      className="object-contain rounded-lg"
+      className='object-contain rounded-lg'
     />
   );
 }
@@ -28,9 +28,7 @@ export const PortableTextComponents: Partial<PortableTextReactComponents> = {
   },
   marks: {
     link: ({ children, value }) => {
-      const rel = !value.href.startsWith("/")
-        ? "noreferrer noopener"
-        : undefined;
+      const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined;
       return (
         <Link href={value.href} rel={rel}>
           {children}
@@ -39,37 +37,19 @@ export const PortableTextComponents: Partial<PortableTextReactComponents> = {
     },
   },
   block: {
-    p: ({ children }) => (
-      <p className="text-lg text-text-light mb-4">{children}</p>
-    ),
-    normal: ({ children }) => (
-      <p className="text-lg text-text-light mb-4">{children}</p>
-    ),
-    h1: ({ children }) => (
-      <h1 className="text-5xl text-moss-600 mb-6 leading-tight">{children}</h1>
-    ),
-    h2: ({ children }) => (
-      <h2 className="text-4xl mt-12 text-moss-500 mb-4">{children}</h2>
-    ),
-    h3: ({ children }) => (
-      <h3 className="text-3xl text-moss-500 mb-3">{children}</h3>
-    ),
+    p: ({ children }) => <p className='text-lg text-text-light mb-4'>{children}</p>,
+    normal: ({ children }) => <p className='text-lg text-text-light mb-4'>{children}</p>,
+    h1: ({ children }) => <h1 className='text-5xl text-moss-600 mb-6 leading-tight'>{children}</h1>,
+    h2: ({ children }) => <h2 className='text-4xl mt-12 text-moss-500 mb-4'>{children}</h2>,
+    h3: ({ children }) => <h3 className='text-3xl text-moss-500 mb-3'>{children}</h3>,
     blockquote: ({ children }) => (
-      <blockquote className="border-l-4 pl-4 text-text-light text-lg italic mb-6">
-        {children}
-      </blockquote>
+      <blockquote className='border-l-4 pl-4 text-text-light text-lg italic mb-6'>{children}</blockquote>
     ),
   },
   list: {
-    bullet: ({ children }) => (
-      <ul className="list-disc list-outside pl-5 overflow-clip text-ellipsis">
-        {children}
-      </ul>
-    ),
+    bullet: ({ children }) => <ul className='list-disc list-outside pl-5 overflow-clip text-ellipsis'>{children}</ul>,
     number: ({ children }) => (
-      <ol className="list-decimal list-outside pl-5 overflow-clip text-ellipsis">
-        {children}
-      </ol>
+      <ol className='list-decimal list-outside pl-5 overflow-clip text-ellipsis'>{children}</ol>
     ),
   },
 };
--- src/components/ui/cards/articleImage.tsx
+++ src/components/ui/cards/articleImage.tsx
@@ -1,21 +1,17 @@
-import { urlFor } from "@/libs/sanity/fetch";
-import { Article } from "@/types/sanity/sanity.types";
-import Image from "next/image";
+import { urlFor } from '@/libs/sanity/client';
+import { Article } from '@/types/sanity/sanity.types';
+import Image from 'next/image';
 
-export async function ArticleImageCard({
-  image,
-}: {
-  image: NonNullable<Article["images"]>[number];
-}) {
+export async function ArticleImageCard({ image }: { image: NonNullable<Article['images']>[number] }) {
   return (
-    <div className="group relative h-64 aspect-video overflow-hidden">
+    <div className='group relative h-64 aspect-video overflow-hidden'>
       <Image
-        src={image ? (await urlFor(image)).url() : "/"}
-        alt="img"
+        src={image ? (await urlFor(image)).url() : '/'}
+        alt='img'
         fill
-        className="w-full h-full object-cover rounded-xl shadow-md group-hover:object-contain group-hover:scale-100 group-hover:shadow-xl transition-all duration-300"
+        className='w-full h-full object-cover rounded-xl shadow-md group-hover:object-contain group-hover:scale-100 group-hover:shadow-xl transition-all duration-300'
       />
-      <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-300" />
+      <div className='absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-300' />
     </div>
   );
 }
