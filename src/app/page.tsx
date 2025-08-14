4aa748b8ed6ede4f1e4bb1b196671ddb913fabfa
kristoffer <kunkristoffer@gmail.com>
2025-08-14 15:23:21 +0200

    fixing InformationSection export and heading type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { wpFetchPostsPaginatedServer } from '@/lib/apollo/server/articles/postsP
 import Hero from '../components/layout/navigation/hero/Hero';
 import Partnere from '../components/Partnere';
 import Fasiliteter from '../components/Fasiliteter';
-import InformationSection from '@/components/Informasjon';
+import { InformationSection } from '@/components/Informasjon';
 import { ContactForm } from '@/components/forms/ContactForm';
 import { ArticlesContainer } from '@/components/layout/articles/container';
 import { UpcomingEvents } from '@/components/layout/events/upcomingEvents';
