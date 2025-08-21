0a81c8fdca608b786c44a560283761037ff96f5e
kristoffer <kunkristoffer@gmail.com>
2025-08-21 09:34:50 +0200

    Improved logging in sanity revalidateTag route

--- src/app/api/sanity/revalidate/tag/route.ts
+++ src/app/api/sanity/revalidate/tag/route.ts
@@ -1,21 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
-import { type NextRequest, NextResponse } from 'next/server';
 import { parseBody } from 'next-sanity/webhook';
 
-type WebhookPayload = {
-  tags: string[];
-};
-
 export async function POST(req: NextRequest) {
+  // Log webhook attemp
+  console.info('[Sanity revalidate tag api route]', { message: 'New revalidate request', origin: req.url });
+
   try {
+    // Load webhook secret
     if (!process.env.SANITY_WEBHOOK_KEY) {
+      console.error('[Sanity revalidate tag api route]', { error: 'Webook initialized without secret key' });
       return new Response('Missing environment variable SANITY_REVALIDATE_SECRET', { status: 500 });
     }
 
-    const { isValidSignature, body } = await parseBody<WebhookPayload>(req, process.env.SANITY_WEBHOOK_KEY, true);
+    // Destructure body from webhook request
+    const { isValidSignature, body } = await parseBody<{ tags: string[] }>(req, process.env.SANITY_WEBHOOK_KEY, true);
 
     if (!isValidSignature) {
       const message = 'Invalid signature';
+      console.error('[Sanity revalidate tag api route]', { error: 'Route called without propper secret' });
       return new Response(JSON.stringify({ message, isValidSignature, body }), {
         status: 401,
       });
@@ -24,6 +27,7 @@ export async function POST(req: NextRequest) {
       return new Response(JSON.stringify({ message, body }), { status: 400 });
     }
 
+    // Revalidate tags
     body.tags.forEach((tag) => {
       revalidateTag(tag);
     });
@@ -31,6 +35,7 @@ export async function POST(req: NextRequest) {
     console.info('[Sanity revalidate tag api route]', { ok: true, tags: body.tags });
     return NextResponse.json({ body });
   } catch (error) {
+    // Catch any runtime errors during revalidation
     console.error('[Sanity revalidate tag api route]', { error });
     return new Response((error as Error).message, { status: 500 });
   }
